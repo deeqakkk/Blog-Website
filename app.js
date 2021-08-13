34fd3fcc03a9a126ad7c5bd745dfa69da6b791bb
Deepak Verma <0822deepakverma@gmail.com>
2021-08-13 23:30:49 +0530

    Deployed

--- app.js
+++ app.js
@@ -5,44 +5,59 @@ const {
 const express = require("express");
 const app = express();
 var _ = require("lodash")
-
+const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+const {MongoClient} = require('mongodb');
+// const client = new MongoClient(uri);
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 
-let urlLink = "";
 
 const posts = [];
 const homeStartingContent = "Following are some of the blogs, which are so much appreciated by our readers around the world.";
 const aboutContent = "We aim to provide a free platform to all the writers and readers around the globe. So that, they can learn and grow together while taking others in there journey of success.";
 const contactContent = "It's nice seeing you here, drop your query. We will get back on you soon.";
 
-app.get("/", function (req, res) {
-    res.render("home", {
-        ejsHomeContent: homeStartingContent,
-        ejsPosts: posts
-    });
+mongoose.connect('mongodb+srv://deeqakkk:'+mongo_password+'@blogwebsite.sycan.mongodb.net/daily-journal', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-// Route Parameter
+
+const blogSchema={
+    title:String,
+    content:String
+};
+const Blog=mongoose.model("Blog",blogSchema);
+
+app.get("/", function (req, res) {
+    Blog.find(function(err,blogs){
+        if(!err)
+        res.render("home", {
+            ejsHomeContent: homeStartingContent,
+            ejsPosts: blogs
+        });
+        else console.log(err);
+    });
+    
+});
+
 
 app.get("/posts/:postName", function (req, res) {
     let testCase = req.params.postName;
-    
-    var testCases=_.lowerCase(testCase);
-    posts.forEach(post => {
-        if (post.postTitle === testCases)
-            {
-                res.render("post",{
-                    ejsPostHeading:post.postTitle,
-                    ejsPostBody:post.postContent
-                });
-            }    ;
-            
-            
+
+    var testCases = _.lowerCase(testCase);
+    Blog.findOne({title:testCases},function(err,blogFound){
+        if(!err)
+        res.render("post", {
+            ejsPostHeading: blogFound.title,
+            ejsPostBody: blogFound.content
+        });
+        else console.log(err);
     })
+   
 });
 
 
@@ -64,14 +79,19 @@ app.post("/createBlog", function (req, res) {
     res.redirect("/compose");
 });
 app.post("/compose", function (req, res) {
-    const newPost = {
-        postTitle: req.body.ejsPostTitle,
-        postContent: req.body.ejsPostContent
-    };
-    posts.push(newPost);
+    const newBlog =new Blog ({
+        title: req.body.ejsPostTitle,
+        content: req.body.ejsPostContent
+    });
+    newBlog.save();
+    // posts.push(newPost);
     res.redirect("/");
 });
 
-app.listen(3000, function (req, res) {
-    console.log("Server running on Port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function(){
+    console.log("Server running on port 3000")
 });
